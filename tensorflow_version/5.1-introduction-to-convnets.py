73216e7d8e113633f56979f191217562a9828e59
zkl <zhangkaileina@163.com>
2019-07-07 22:12:28 +0800

    Update 5.1-introduction-to-convnets.py

--- tensorflow_version/5.1-introduction-to-convnets.py
+++ tensorflow_version/5.1-introduction-to-convnets.py
@@ -45,31 +45,55 @@ model.add(layers.Dense(64, activation='relu'))
 model.add(layers.Dense(10, activation='softmax'))
 
 model.summary()
+#Model: "sequential"
+#_________________________________________________________________
+#Layer (type)                 Output Shape              Param #   
+#=================================================================
+#conv2d (Conv2D)              (None, 26, 26, 32)        320       
+#_________________________________________________________________
+#max_pooling2d (MaxPooling2D) (None, 13, 13, 32)        0         
+#_________________________________________________________________
+#conv2d_1 (Conv2D)            (None, 11, 11, 64)        18496     
+#_________________________________________________________________
+#max_pooling2d_1 (MaxPooling2 (None, 5, 5, 64)          0         
+#_________________________________________________________________
+#conv2d_2 (Conv2D)            (None, 3, 3, 64)          36928     
+#_________________________________________________________________
+#flatten (Flatten)            (None, 576)               0         
+#_________________________________________________________________
+#dense (Dense)                (None, 64)                36928     
+#_________________________________________________________________
+#dense_1 (Dense)              (None, 10)                650       
+#=================================================================
+#Total params: 93,322
+#Trainable params: 93,322
+#Non-trainable params: 0
+#_________________________________________________________________
 
 
-from keras.datasets import mnist
-from keras.utils import to_categorical
+from tensorflow.keras.datasets import mnist
+from tensorflow.python.keras.utils.np_utils import to_categorical
 
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
+# 更改大小
 train_images = train_images.reshape((60000, 28, 28, 1))
 train_images = train_images.astype('float32') / 255
 
 test_images = test_images.reshape((10000, 28, 28, 1))
 test_images = test_images.astype('float32') / 255
 
+# one-hot编码
 train_labels = to_categorical(train_labels)
 test_labels = to_categorical(test_labels)
-
+# 使用交叉熵损失函数
 model.compile(optimizer='rmsprop',
               loss='categorical_crossentropy',
               metrics=['accuracy'])
 model.fit(train_images, train_labels, epochs=5, batch_size=64)
 
-
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 
-
 test_acc
-
+# 0.9914
 
